5c8bc76f0f19c27b3e512ec8525b97d53e48ce67
Vincent Sanders <vincesanders@hotmail.com>
2020-02-18 15:38:21 -0600

    Added post endpoint.

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -22,6 +22,18 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+    db('cars').insert(req.body).then(ids => {
+        getByID(ids[0]).then(cars => {
+            res.status(201).json(cars[0]);
+        }).catch(err => {
+            res.status(500).json({ error: 'Unable to retrieve the car that was just added.\nCheck inventory to ensure it was added correctly.' });
+        });
+    }).catch(err => {
+        res.status(500).json({ message: 'Unable to add the vehicle to the database.' });
+    });
+});
+
 function getByID(id) {
     return db('cars').where({ id: id });
 }
