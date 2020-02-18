54528261e94b6c9e76b1fee2eac0a07ccc13a032
Vincent Sanders <vincesanders@hotmail.com>
2020-02-18 15:52:37 -0600

    Added helmet for added security. Added a logger middleware.

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
-
+const helmet = require('helmet');
 const carsRouter = require('./cars/carsRouter');
 
 const server = express();
 
-server.use(express.json());
+server.use(express.json(), logger, helmet());
 
 server.use('/api/cars', carsRouter);
 
 server.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the car dealer API.' });
+    res.status(200).send(`<h1>Welcome to the car dealer API.</h1>`);
 });
+
+function logger(req, res, next) {
+    console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} ${req.get('Origin')}`);
+    next();
+}
 
 module.exports = server;
